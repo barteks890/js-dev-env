b90f4890f9106db98c9dc53976fb9d886a969393
Bartek <bartek@bartek.pl>
2017-04-19 18:53:10 +0200

    Podlaczenie Babela (w tym narzedzia 'babel-node' do uruchamiania skryptow)

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
-var chalk = require('chalk'); // wtyczka 'chalk' pozwala na kolorowanie tekstu w konsoli
+import chalk from 'chalk'; // wtyczka 'chalk' pozwala na kolorowanie tekstu w konsoli
 
 console.log(chalk.green('Starting app in dev mode...'));
